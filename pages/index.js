e08929edd394e58ad38dd7a2d3804926f4343372
Jefferson Louis <137832123+jeffersonlouissh@users.noreply.github.com>
2026-04-27 13:59:49 +0000

    First Commit, Home page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <h1>Oii Amor ❤️</h1>
+      <h3> Home Pages</h3>
+      <h2>I'm back</h2>{" "}
+    </>
+  );
+}
+
+function test() {
+  console.log("log de tests");
+}
+export default Home;
