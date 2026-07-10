781420cdf9acde80d48c3dd618137707bad0c988
Jefferson Louis <137832123+jeffersonlouissh@users.noreply.github.com>
2026-07-10 18:47:10 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 3 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ nome: "Jefferson" });
-}
+  const updated_at = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
 
+  let version = await database.query("SHOW server_version;");
+  let max_connections = await database.query("SHOW max_connections;");
+  let opened_connections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(max_connections.rows[0].max_connections),
+        opened_connections: opened_connections.rows[0].count,
+      },
+    },
+  });
+}
 export default status;
