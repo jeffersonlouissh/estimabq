2b441ad9240f90a9228a9271fe29bff353e71a55
Jefferson Louis <137832123+jeffersonlouissh@users.noreply.github.com>
2026-07-11 12:49:38 +0000

    makes client.connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
-  await client.connect();
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
+    await client.connect();
     const response = await client.query(queryObject);
     return response;
   } catch (err) {
     console.error(err);
+    throw err;
   } finally {
     await client.end();
   }
